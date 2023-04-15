e82cfcbf037b1de0048f26ba2882fb737137a474
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-04-15 21:08:23 +0530

    change db url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.ATLAS_URI,{
+        const conn = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
         });
         console.log(`MongoDB connected: ${conn.connection.host}`);
