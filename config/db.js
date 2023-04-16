48ca94cd62ac66b9ef250bdb2c0c992ee30ec71b
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-04-16 08:12:24 +0530

    fix: change dburl

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    const dbUrl = process.env.DB_URL || "mongodb://localhost/notes";
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
+        const conn = await mongoose.connect(dbUrl,{
             useNewUrlParser: true,
         });
         console.log(`MongoDB connected: ${conn.connection.host}`);
